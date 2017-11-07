4c0202e28b42f5d0b1aa4d907f54eceb37394bd5
alxdc <alxdc@protonmail.com>
2017-11-07 15:33:42 +0100

    Code cleanup and translation

--- movies-infos.py
+++ movies-infos.py
@@ -1,27 +1,53 @@
 import os
 import subprocess
 import csv
+import argparse
+
 FNULL = open(os.devnull, 'w')
+mediaInfo = os.path.join('bin', 'MediaInfo')
+params = {'size': 'General;%FileSize%',
+          'format': 'General;%Format%',
+          'width': 'Video;%Width%',
+          'codec': 'Video;%Format%',
+          'audioformat': 'Audio;%Format%',
+          'audiocodec': 'Audio;%CodecID%'}
 
-path = input("Entrer chemin Films: ")
-list = []
-for root, dirs, files in sorted(os.walk(path)):
-    for file in files:
-        if file.endswith(".mkv"):
-            fullPath = os.path.join(path, root, file)
-            print(fullPath)
-            size = subprocess.check_output('mediainfo --Inform="General;%FileSize%"' + ' "' + fullPath).decode("utf-8")[:-2]
-            format = subprocess.check_output('mediainfo --Inform="General;%Format%"' + ' "' + fullPath + '"').decode("utf-8")[:-2]
-            width = subprocess.check_output('mediainfo --Inform="Video;%Width%"' + ' "' + fullPath + '"').decode("utf-8")[:-2]
-            codec = subprocess.check_output('mediainfo --Inform="Video;%Format%"' + ' "' + fullPath + '"').decode("utf-8")[:-2]
-            audioformat = subprocess.check_output('mediainfo --Inform="Audio;%Format%"' + ' "' + fullPath + '"').decode("utf-8")[:-2]
-            audiocodec = subprocess.check_output('mediainfo --Inform="Audio;%CodecID%"' + ' "' + fullPath + '"').decode("utf-8")[:-2]
-            list.append({'nom': file, 'taille': size, 'format': format, 'largeur': width, 'codec': codec, 'format_audio': audioformat, 'codec_audio': audiocodec})
+def scan(dir):
+    dir = os.path.abspath(dir)
+    list = []
+    for root, dirs, files in sorted(os.walk(dir)):
+        for file in files:
+            if file.endswith(".mkv"):
+                fullPath = ' "' + os.path.join(dir, root, file) + '"'
+                print(fullPath)
+                fullPath = ' "' + fullPath + '"'
+                infos = {'name': file}
+                for name, arg in params.items():
+                    fct = '--Inform='
+                    cmd = mediaInfo + ' --Inform=' + arg + ' "' + fullPath + '"'
+                    infos[name] = subprocess.check_output(cmd).decode('utf-8')[:-2]
 
-print(list)
+                list.append(infos)
+                print(infos)
+                print()
+    
+    return list
 
-keys = list[0].keys()
-with open('films.csv', 'w', newline='') as output_file:
-    dict_writer = csv.DictWriter(output_file, keys, delimiter=';')
-    dict_writer.writeheader()
-    dict_writer.writerows(list)
+def writeCSV(list):
+    keys = list[0].keys()
+    with open('movies-infos.csv', 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys, delimiter=';')
+        dict_writer.writeheader()
+        dict_writer.writerows(list)
+    
+def parseArgs():
+    parser = argparse.ArgumentParser(description='Scans all movies in a given folder and outputs a CSV file with required detauls about the movies')
+
+    parser.add_argument('dir', help='Directory to be scanned')
+    
+    return parser.parse_args()
+    
+if __name__ == '__main__':
+    list = scan(parseArgs().dir)
+    writeCSV(list)
+    print('Done')
